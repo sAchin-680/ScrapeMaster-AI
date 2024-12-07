07cf4e98c286651e9c9bab06c33c45d34ddd2384
sAchin-680 <mrmister680@gmail.com>
2024-12-07 14:00:15 +0530

    Implement cron job for product price updates and email notifications; add utility functions for price calculations

---
+++ app/api/cron/route.ts
@@ -0,0 +1,61 @@
+import Product from "@/lib/models/product.model";
+import { connectDB } from "@/lib/mogoose";
+import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
+import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        await connectDB();
+
+        const products = await Product.find({});
+        if (!products) throw new Error("No products found");
+
+        const updatedProducts = await Promise.all(
+            products.map(async (currentProduct) => {
+                const scrappedProduct = await currentProduct.scrapping(currentProduct.url);
+                if (!scrappedProduct) throw new Error("No product found");
+
+                const updatedPriceHistory = [
+                    ...currentProduct.priceHistory,
+                    { price: scrappedProduct.currentPrice },
+                ];
+
+                const product = {
+                    ...scrappedProduct,
+                    priceHistory: updatedPriceHistory,
+                    lowestPrice: getLowestPrice(updatedPriceHistory),
+                    highestPrice: getHighestPrice(updatedPriceHistory),
+                    averagePrice: getAveragePrice(updatedPriceHistory),
+                };
+
+                const updatedProduct = await Product.findOneAndUpdate(
+                    { url: scrappedProduct.url },
+                    product
+                );
+
+                const emailNotification = getEmailNotifType(scrappedProduct, currentProduct);
+
+                if (emailNotification && updatedProduct.users.length > 0) {
+                    const productInfo = {
+                        title: updatedProduct.title,
+                        url: updatedProduct.url,
+                    };
+
+                    const emailContent = await generateEmailBody(productInfo, emailNotification);
+                    const userEmails = updatedProduct.users.map((user: any) => user.email);
+                    await sendEmail(emailContent, userEmails);
+                }
+
+                return updatedProduct;
+            })
+        );
+
+        return NextResponse.json({
+            message: "OK",
+            data: updatedProducts,
+        });
+    } catch (error) {
+        throw new Error(`Error in GET: ${error}`);
+    }
+}
